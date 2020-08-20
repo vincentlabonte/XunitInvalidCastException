345fa79a5be068877d786c63dc2528236cc62e76
Vincent Labonte <vilabont@microsoft.com>
2020-08-20 13:28:11 -0700

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var absoluteAssemblyPath = @"C:\repos\XunitInvalidCastException\Runner\bin\Debug\Runner.dll";
+            Assembly assembly = Assembly.LoadFrom(absoluteAssemblyPath);
+            Type jobType = assembly.GetType("Runner.Runner", throwOnError: true);
+            dynamic job = Activator.CreateInstance(jobType);
+            job.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ Runner/Runner.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit.Runners;
+
+namespace Runner
+{
+    public class Runner
+    {
+        public Runner()
+        {
+            consoleLock = new object();
+            finished = new ManualResetEvent(false);
+            result = 0;
+        }
+
+        public int RunAsync()
+        {
+            var testAssembly = @"C:\repos\XunitInvalidCastException\Test\bin\Debug\Test.dll";
+
+            using (var runner = AssemblyRunner.WithAppDomain(testAssembly))
+            {
+                runner.OnDiscoveryComplete = OnDiscoveryComplete;
+                runner.OnExecutionComplete = OnExecutionComplete;
+                runner.OnTestFailed = OnTestFailed;
+                runner.OnTestSkipped = OnTestSkipped;
+
+                Console.WriteLine("Discovering...");
+                runner.Start();
+
+                finished.WaitOne();
+                finished.Dispose();
+
+                return result;
+            }
+        }
+
+        private void OnDiscoveryComplete(DiscoveryCompleteInfo info)
+        {
+            lock (consoleLock)
+                Console.WriteLine($"Running {info.TestCasesToRun} of {info.TestCasesDiscovered} tests...");
+        }
+
+        private void OnExecutionComplete(ExecutionCompleteInfo info)
+        {
+            lock (consoleLock)
+                Console.WriteLine($"Finished: {info.TotalTests} tests in {Math.Round(info.ExecutionTime, 3)}s ({info.TestsFailed} failed, {info.TestsSkipped} skipped)");
+
+            finished.Set();
+        }
+
+        private void OnTestFailed(TestFailedInfo info)
+        {
+            lock (consoleLock)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("[FAIL] {0}: {1}", info.TestDisplayName, info.ExceptionMessage);
+                if (info.ExceptionStackTrace != null)
+                    Console.WriteLine(info.ExceptionStackTrace);
+
+                Console.ResetColor();
+            }
+
+            result = 1;
+        }
+
+        private void OnTestSkipped(TestSkippedInfo info)
+        {
+            lock (consoleLock)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("[SKIP] {0}: {1}", info.TestDisplayName, info.SkipReason);
+                Console.ResetColor();
+            }
+        }
+
+        // We use consoleLock because messages can arrive in parallel, so we want to make sure we get
+        // consistent console output.
+        private object consoleLock { get; }
+
+        // Use an event to know when we're done
+        private ManualResetEvent finished { get; }
+
+        // Start out assuming success; we'll set this to 1 if we get a failed test
+        private int result { get; set; }
+    }
+}
